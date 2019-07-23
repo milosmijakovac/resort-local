461ff96c5b9b68b6bb44200858b8e2006e479848
Milos <skapha@gmail.com>
2019-07-23 18:19:55 +0200

    fixes

--- src/context.js
+++ src/context.js
@@ -11,7 +11,7 @@ class RoomProvider extends Component {
     rooms: [],
     sortedRooms: [],
     featuredRooms: [],
-    loading: false,
+    loading: true,
     type: "all",
     capacity: 1,
     price: 0,
