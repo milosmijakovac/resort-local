8906ff6e4fefe6cba21ae500230d1d4201d1a521
Milos <skapha@gmail.com>
2019-08-23 14:06:52 +0200

    css classes fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,36 +6,34 @@ import { Link } from "react-router-dom";
 class Navbar extends Component {
   state = {
     isOpen: false
-  }
-
+  };
 
   handleToggle = () => {
-    this.setState({isOpen: !this.state.isOpen})
-  }
-
+    this.setState({ isOpen: !this.state.isOpen });
+  };
 
   render() {
-   
-    return <nav className="navbar">
-<div className="nave-center"></div>
-<div className="nave-header">
-  <Link to="/">
-    <img src={logo} alt="beach resort"/>
-    </Link>
-    <button type="button" className="nav-btn" onClick={this.handleToggle}>
-      <FaAlignRight className="nav-icon" />
-    </button>
-</div>
-<ul className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}>
-<li>
-  <Link to="/">Home</Link>
-</li>
-<li>
-  <Link to="/rooms">Rooms</Link>
-</li>
-</ul>
-
-    </nav>;
+    return (
+      <nav className="navbar">
+        <div className="nav-center" />
+        <div className="nav-header">
+          <Link to="/">
+            <img src={logo} alt="beach resort" />
+          </Link>
+          <button type="button" className="nav-btn" onClick={this.handleToggle}>
+            <FaAlignRight className="nav-icon" />
+          </button>
+        </div>
+        <ul className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/rooms">Rooms</Link>
+          </li>
+        </ul>
+      </nav>
+    );
   }
 }
 
